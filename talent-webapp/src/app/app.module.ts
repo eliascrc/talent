70d71108d004484472168c3407fae3fd690fe3fd
Elías Calderón <eliascr98@outlook.com>
2018-03-26 11:08:26 -0600

    TAL-142 Setup Web Platform Project
    Added Ng-Bootstrap, fixed problems with app.component.html and added the test routes and components

--- talent-webapp/src/app/app.module.ts
+++ talent-webapp/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
