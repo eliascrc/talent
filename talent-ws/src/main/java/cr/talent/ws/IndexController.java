d48129ce3d656a7d9d361baa8b117763b3d0c549
otto.mena <otmerk1@gmail.com>
2018-03-29 00:33:43 -0600

    added the web.xml, talent-ws-mvc.spring.xml and index.jspx for testing the IndexController.java

---
+++ talent-ws/src/main/java/cr/talent/ws/IndexController.java
@@ -0,0 +1,16 @@
+package cr.talent.web.controllers ;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
