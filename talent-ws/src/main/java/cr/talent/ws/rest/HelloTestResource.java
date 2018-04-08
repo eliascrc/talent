db4ff0d83d0d53f1f3294ed0e06698815b0c266b
Rodrigo Bartels <rodrigobartels@gmail.com>
2018-04-07 18:30:36 -0600

    TAL-152: BE | Setup Maven Tomcat Plugin
    Fixed issue with the dependencies caused by the unnecesary usage of hibernate-validator.

--- talent-ws/src/main/java/cr/talent/ws/rest/HelloTestResource.java
+++ talent-ws/src/main/java/cr/talent/ws/rest/HelloTestResource.java
@@ -1,11 +1,16 @@
-package cr.talent.ws;
+package cr.talent.ws.rest;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+@Component
+@Scope("request")
 @Path("/test")
-public class HelloTest {
+public class HelloTestResource {
 
     @GET
     public Response test() {
