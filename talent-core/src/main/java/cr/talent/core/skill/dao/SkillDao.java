9a4321a95d201808abd58fddd5f82498b87053f4
Elías Calderón <eliascr98@outlook.com>
2018-04-16 22:22:16 -0600

    TAL-206 | BE | Skill Service
    Added the Services and DAO classes for Skill.

---
+++ talent-core/src/main/java/cr/talent/core/skill/dao/SkillDao.java
@@ -0,0 +1,13 @@
+package cr.talent.core.skill.dao;
+
+import cr.talent.support.dao.CrudDao;
+
+/**
+ * Data access object for all the {@link Skill} related operations.
+ *
+ * @author Elías Calderón
+ */
+public interface SkillDao extends CrudDao<Skill, String> {
+
+
+}
---
+++ talent-core/src/main/java/cr/talent/core/skill/service/SkillService.java
@@ -0,0 +1,13 @@
+package cr.talent.core.skill.service;
+
+import cr.talent.support.service.CrudService;
+
+/**
+ * Provides business logic services related to {@link Skill} entities.
+ *
+ * @author Elías Calderón
+ */
+public interface SkillService extends CrudService<Skill, String> {
+
+
+}
