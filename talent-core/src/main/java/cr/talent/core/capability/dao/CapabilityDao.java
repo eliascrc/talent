a692dba18b22dba151045a121e8d865c7e05c1d6
Elías Calderón <eliascr98@outlook.com>
2018-04-16 22:13:45 -0600

    TAL-205 | BE | Capability Service
    Added the Services and DAO classes for Capability.

---
+++ talent-core/src/main/java/cr/talent/core/capability/dao/CapabilityDao.java
@@ -0,0 +1,13 @@
+package cr.talent.core.capability.dao;
+
+import cr.talent.support.dao.CrudDao;
+
+/**
+ * Data access object for all the {@link Capability} related operations.
+ *
+ * @author Elías Calderón
+ */
+public interface CapabilityDao extends CrudDao<Capability, String> {
+
+
+}
---
+++ talent-core/src/main/java/cr/talent/core/capability/service/CapabilityService.java
@@ -0,0 +1,13 @@
+package cr.talent.core.capability.service;
+
+import cr.talent.support.service.CrudService;
+
+/**
+ * Provides business logic services related to {@link Capability} entities.
+ *
+ * @author Elías Calderón
+ */
+public interface CapabilityService extends CrudService<Capability, String> {
+
+
+}
