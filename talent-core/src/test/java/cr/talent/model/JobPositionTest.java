dc04201599e732b7af35d11116a6a83f94dfb69d
JosueCubero <jo96cube@gmail.com>
2018-04-22 12:17:47 -0600

    TAL-184 | BE | Class Model Test Classes

--- talent-core/src/test/java/cr/talent/model/JobPositionTest.java
+++ talent-core/src/test/java/cr/talent/model/JobPositionTest.java
@@ -1,4 +1,170 @@
 package cr.talent.model;
 
-public class JobPositionTest {
+import java.util.Date;
+import java.util.HashSet;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+
+/**
+ * Class that allows to test the jobPosition methods to know all the different paths they could take.
+ * This class contains the test of the inherited methods
+ * from {@link cr.talent.model.BasicEntity} class.
+ *
+ * @author Josue Cubero
+ */
+public class JobPositionTest{
+
+    private static final String ID = "1234";
+    private static final String ID2 = "12345";
+    private static final OrganizationCapabilityLevel ORGANIZATION_CAPABILITY_LEVEL = mock(OrganizationCapabilityLevel.class);
+    private static final OrganizationCapabilityLevel ORGANIZATION_CAPABILITY_LEVEL2 = mock(OrganizationCapabilityLevel.class);
+
+    @Test
+    public void coreTest() {
+
+        Invitation invitation = mock(Invitation.class);
+        TechnicalResource technicalResource = mock(TechnicalResource.class);
+        Date entityCreationTimestamp = new Date();
+        Date lastUpdatedTimestamp = new Date();
+        long entityVersion = 1l;
+        Date startDate = new Date();
+        Date endDate = new Date();
+
+        // Verify Constructor
+        JobPosition jobPosition = new JobPosition();
+
+        // Verify the sets
+        jobPosition.setId(ID);
+        jobPosition.setEntityCreationTimestamp(entityCreationTimestamp);
+        jobPosition.setLastUpdatedTimestamp(lastUpdatedTimestamp);
+        jobPosition.setEntityVersion(entityVersion);
+        jobPosition.setStartDate(startDate);
+        jobPosition.setEndDate(endDate);
+        jobPosition.setInvitation(invitation);
+        jobPosition.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+        jobPosition.setTechnicalResource(technicalResource);
+
+        // Verify the gets
+        assertEquals(ID, jobPosition.getId());
+        assertEquals(entityCreationTimestamp, jobPosition.getEntityCreationTimestamp());
+        assertEquals(lastUpdatedTimestamp, jobPosition.getLastUpdatedTimestamp());
+        assertEquals(entityVersion, jobPosition.getEntityVersion());
+        assertEquals(startDate, jobPosition.getStartDate());
+        assertEquals(endDate, jobPosition.getEndDate());
+        assertEquals(invitation, jobPosition.getInvitation());
+        assertEquals(ORGANIZATION_CAPABILITY_LEVEL, jobPosition.getOrganizationCapabilityLevel());
+        assertEquals(technicalResource, jobPosition.getTechnicalResource());
+    }
+
+    //ON EQUALS TESTS.
+
+    @Test
+    public void testEqualForSameObject() {
+        JobPosition jobPosition1 = new JobPosition();
+
+        assertTrue(jobPosition1.equals(jobPosition1));
+    }
+
+    @Test
+    public void testEqualForDifferentClass() {
+        JobPosition jobPosition1 = new JobPosition();
+
+        assertFalse(jobPosition1.equals(new Object()));
+    }
+
+    @Test
+    public void testEqualForPersistentJobPosition() {
+        JobPosition jobPosition1 = new JobPosition();
+        jobPosition1.setId(ID);
+
+        JobPosition jobPosition2 = new JobPosition();
+        jobPosition2.setId(ID);
+
+        assertTrue(jobPosition1.equals(jobPosition2));
+    }
+
+    @Test
+    public void testNonEqualForPersistentJobPosition() {
+        JobPosition jobPosition1 = new JobPosition();
+        jobPosition1.setId(ID);
+
+        JobPosition jobPosition2 = new JobPosition();
+        jobPosition2.setId(ID2);
+
+        assertFalse(jobPosition1.equals(jobPosition2));
+    }
+
+    @Test
+    public void testEqualForNonPersistentInvitation() {
+        JobPosition jobPosition1 = new JobPosition();
+        jobPosition1.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        JobPosition jobPosition2 = new JobPosition();
+        jobPosition2.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        assertTrue(jobPosition1.equals(jobPosition2));
+    }
+
+    @Test
+    public void testNonEqualForNonPersistentInvitation() {
+        JobPosition jobPosition1 = new JobPosition();
+        jobPosition1.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        JobPosition jobPosition2 = new JobPosition();
+        jobPosition2.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL2);
+
+        assertFalse(jobPosition1.equals(jobPosition2));
+    }
+
+    //ON HASH TESTS.
+
+    @Test
+    public void testEqualHashCodeForPersistentJobPosition() {
+        JobPosition jobPosition1 = new JobPosition();
+        jobPosition1.setId(ID);
+
+        JobPosition jobPosition2 = new JobPosition();
+        jobPosition2.setId(ID);
+
+        assertTrue(jobPosition1.hashCode() == jobPosition2.hashCode());
+    }
+
+    @Test
+    public void testNonEqualHashCodeForPersistentJobPosition() {
+        JobPosition jobPosition1 = new JobPosition();
+        jobPosition1.setId(ID);
+
+        JobPosition jobPosition2 = new JobPosition();
+        jobPosition2.setId(ID2);
+
+        assertFalse(jobPosition1.hashCode() == jobPosition2.hashCode());
+    }
+
+    @Test
+    public void testEqualHashCodeForNonPersistentinvitation() {
+        JobPosition jobPosition1 = new JobPosition();
+        jobPosition1.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        JobPosition jobPosition2 = new JobPosition();
+        jobPosition2.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        assertTrue(jobPosition1.hashCode() == jobPosition2.hashCode());
+    }
+
+    @Test
+    public void testNonEqualHashCodeForNonPersistentinvitation() {
+        JobPosition jobPosition1 = new JobPosition();
+        jobPosition1.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        JobPosition jobPosition2 = new JobPosition();
+        jobPosition2.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL2);
+
+        assertFalse(jobPosition1.hashCode() == jobPosition2.hashCode());
+    }
 }
--- talent-core/src/test/java/cr/talent/model/TechnicalPositionTest.java
+++ talent-core/src/test/java/cr/talent/model/TechnicalPositionTest.java
@@ -1,4 +1,170 @@
 package cr.talent.model;
 
-public class TechnicalPositionTest {
+import java.util.Date;
+import java.util.HashSet;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+
+/**
+ * Class that allows to test the technicalPosition methods to know all the different paths they could take.
+ * This class contains the test of the inherited methods
+ * from {@link cr.talent.model.BasicEntity} class.
+ *
+ * @author Josue Cubero
+ */
+public class TechnicalPositionTest{
+
+    private static final String ID = "1234";
+    private static final String ID2 = "12345";
+    private static final OrganizationCapabilityLevel ORGANIZATION_CAPABILITY_LEVEL = mock(OrganizationCapabilityLevel.class);
+    private static final OrganizationCapabilityLevel ORGANIZATION_CAPABILITY_LEVEL2 = mock(OrganizationCapabilityLevel.class);
+
+    @Test
+    public void coreTest() {
+
+        CareerPath careerPath = mock(CareerPath.class);
+        Invitation invitation = mock(Invitation.class);
+        Date entityCreationTimestamp = new Date();
+        Date lastUpdatedTimestamp = new Date();
+        long entityVersion = 1l;
+        Date startDate = new Date();
+        Date endDate = new Date();
+
+        // Verify Constructor
+        TechnicalPosition technicalPosition = new TechnicalPosition();
+
+        // Verify the sets
+        technicalPosition.setId(ID);
+        technicalPosition.setEntityCreationTimestamp(entityCreationTimestamp);
+        technicalPosition.setLastUpdatedTimestamp(lastUpdatedTimestamp);
+        technicalPosition.setEntityVersion(entityVersion);
+        technicalPosition.setCareerPath(careerPath);
+        technicalPosition.setInvitation(invitation);
+        technicalPosition.setStartDate(startDate);
+        technicalPosition.setEndDate(endDate);
+        technicalPosition.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        // Verify the gets
+        assertEquals(ID, technicalPosition.getId());
+        assertEquals(entityCreationTimestamp, technicalPosition.getEntityCreationTimestamp());
+        assertEquals(lastUpdatedTimestamp, technicalPosition.getLastUpdatedTimestamp());
+        assertEquals(entityVersion, technicalPosition.getEntityVersion());
+        assertEquals(careerPath, technicalPosition.getCareerPath());
+        assertEquals(invitation, technicalPosition.getInvitation());
+        assertEquals(startDate, technicalPosition.getStartDate());
+        assertEquals(endDate, technicalPosition.getEndDate());
+        assertEquals(ORGANIZATION_CAPABILITY_LEVEL, technicalPosition.getOrganizationCapabilityLevel());
+    }
+
+    //ON EQUALS TESTS.
+
+    @Test
+    public void testEqualForSameObject() {
+        TechnicalPosition technicalPosition1 = new TechnicalPosition();
+
+        assertTrue(technicalPosition1.equals(technicalPosition1));
+    }
+
+    @Test
+    public void testEqualForDifferentClass() {
+        TechnicalPosition technicalPosition1 = new TechnicalPosition();
+
+        assertFalse(technicalPosition1.equals(new Object()));
+    }
+
+    @Test
+    public void testEqualForPersistentTechnicalPosition() {
+        TechnicalPosition technicalPosition1 = new TechnicalPosition();
+        technicalPosition1.setId(ID);
+
+        TechnicalPosition technicalPosition2 = new TechnicalPosition();
+        technicalPosition2.setId(ID);
+
+        assertTrue(technicalPosition1.equals(technicalPosition2));
+    }
+
+    @Test
+    public void testNonEqualForPersistentTechnicalPosition() {
+        TechnicalPosition technicalPosition1 = new TechnicalPosition();
+        technicalPosition1.setId(ID);
+
+        TechnicalPosition technicalPosition2 = new TechnicalPosition();
+        technicalPosition2.setId(ID2);
+
+        assertFalse(technicalPosition1.equals(technicalPosition2));
+    }
+
+    @Test
+    public void testEqualForNonPersistentInvitation() {
+        TechnicalPosition technicalPosition1 = new TechnicalPosition();
+        technicalPosition1.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        TechnicalPosition technicalPosition2 = new TechnicalPosition();
+        technicalPosition2.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        assertTrue(technicalPosition1.equals(technicalPosition2));
+    }
+
+    @Test
+    public void testNonEqualForNonPersistentInvitation() {
+        TechnicalPosition technicalPosition1 = new TechnicalPosition();
+        technicalPosition1.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        TechnicalPosition technicalPosition2 = new TechnicalPosition();
+        technicalPosition2.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL2);
+
+        assertFalse(technicalPosition1.equals(technicalPosition2));
+    }
+
+    //ON HASH TESTS.
+
+    @Test
+    public void testEqualHashCodeForPersistentTechnicalPosition() {
+        TechnicalPosition technicalPosition1 = new TechnicalPosition();
+        technicalPosition1.setId(ID);
+
+        TechnicalPosition technicalPosition2 = new TechnicalPosition();
+        technicalPosition2.setId(ID);
+
+        assertTrue(technicalPosition1.hashCode() == technicalPosition2.hashCode());
+    }
+
+    @Test
+    public void testNonEqualHashCodeForPersistentTechnicalPosition() {
+        TechnicalPosition technicalPosition1 = new TechnicalPosition();
+        technicalPosition1.setId(ID);
+
+        TechnicalPosition technicalPosition2 = new TechnicalPosition();
+        technicalPosition2.setId(ID2);
+
+        assertFalse(technicalPosition1.hashCode() == technicalPosition2.hashCode());
+    }
+
+    @Test
+    public void testEqualHashCodeForNonPersistentinvitation() {
+        TechnicalPosition technicalPosition1 = new TechnicalPosition();
+        technicalPosition1.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        TechnicalPosition technicalPosition2 = new TechnicalPosition();
+        technicalPosition2.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        assertTrue(technicalPosition1.hashCode() == technicalPosition2.hashCode());
+    }
+
+    @Test
+    public void testNonEqualHashCodeForNonPersistentinvitation() {
+        TechnicalPosition technicalPosition1 = new TechnicalPosition();
+        technicalPosition1.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL);
+
+        TechnicalPosition technicalPosition2 = new TechnicalPosition();
+        technicalPosition2.setOrganizationCapabilityLevel(ORGANIZATION_CAPABILITY_LEVEL2);
+
+        assertFalse(technicalPosition1.hashCode() == technicalPosition2.hashCode());
+    }
 }
