924bef5aa2a82e610464dc924f8c4d9b786d1ad4
danielmdob <daniel.montesdeocab@gmail.com>
2018-04-30 23:55:30 -0600

    TAL-261 BE | Privacy Policy Web Service
    Made fixes on privacy policy dao, made a fix to the security xml that Josué L suggested

--- talent-ws/src/main/java/cr/talent/ws/rest/PrivacyPolicyResource.java
+++ talent-ws/src/main/java/cr/talent/ws/rest/PrivacyPolicyResource.java
@@ -1,6 +1,8 @@
 package cr.talent.ws.rest;
 
 import cr.talent.core.privacyPolicy.service.PrivacyPolicyService;
+import cr.talent.model.PrivacyPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -10,23 +12,33 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+/**
+ * Resource with a GET endpoint that returns the active privacy policy
+ *
+ * @author Daniel Montes de Oca
+ */
 @Component
 @Scope("request")
-@Path("/privacypolicy")
+@Path("/content/privacy-policy")
 public class PrivacyPolicyResource {
 
-
+    @Autowired
     PrivacyPolicyService privacyPolicyService;
 
+    /**
+     * GET endpoint that returns the active privacy policy
+     * @return a 200 response with the active privacy policy, 204 code if there is none
+     */
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public Response test() {
-        String privacyPolicyContent = privacyPolicyService.getActivePrivacyPolicy().getContent();
+    public Response getActivePrivacyPolicy() {
+        PrivacyPolicy activePrivacyPolicy = privacyPolicyService.getActivePrivacyPolicy();
 
         Response response;
-        if (privacyPolicyContent == null) {
+        if (activePrivacyPolicy == null) {
             response = Response.status(204).build();
         } else {
+            String privacyPolicyContent = activePrivacyPolicy.getContent();
             response = Response.status(200).entity(privacyPolicyContent).build();
         }
 
