bd900f2b5ed83f129afd0cdca1e8bbc678bb8f00
majocubero <majo.qbro@gmail.com>
2018-05-04 23:54:16 -0600

    TAL-247 | BE | CORS Filter Configuration
    Added a Jersey Filter to the web services projet

--- talent-ws/src/main/java/cr/talent/ws/rest/support/CORSFilter.java
+++ talent-ws/src/main/java/cr/talent/ws/rest/support/CORSFilter.java
@@ -4,7 +4,13 @@ import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
 
+/**
+ * CORS (Cross-Origin Resource Sharing) filter configuration.
+ *
+ *@author María José Cubero.
+ */
 public class CORSFilter implements ContainerResponseFilter {
+
     @Override
     public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
 
