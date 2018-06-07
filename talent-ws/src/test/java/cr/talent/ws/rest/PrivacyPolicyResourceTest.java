382e64ff04d4ab3bd42818896854b4c9e1c4e3b7
JosueCubero <jo96cube@gmail.com>
2018-06-06 20:57:20 -0600

    Updated automation of web content to adapt to web services changes.

--- talent-ws/src/test/java/cr/talent/ws/rest/PrivacyPolicyResourceTest.java
+++ talent-ws/src/test/java/cr/talent/ws/rest/PrivacyPolicyResourceTest.java
@@ -10,14 +10,29 @@ public class PrivacyPolicyResourceTest extends FunctionalTest {
     }
 
     @Test
-    public void pingTermsOfService() {
-        given().when().get("/ws/content/privacyPolicy").then().statusCode(200);
+    public void nullPlatformRequestTest(){
+        given().when().get("/ws/content/privacyPolicy").then().statusCode(400);
+    }
+
+    @Test
+    public void emptyPlatformRequestTest(){
+        given().queryParam("platform","").when().get("/ws/content/privacyPolicy").then().statusCode(400);
+    }
+
+    @Test
+    public void testWebPrivacyPolicy() {
+        given().queryParam("platform","web").when().get("/ws/content/privacyPolicy").then().statusCode(200);
+    }
+
+    @Test
+    public void testAndroidPrivacyPolicy() {
+        given().queryParam("platform","android").when().get("/ws/content/privacyPolicy").then().statusCode(200);
     }
 
     //The following test was run on localhost, by setting the active ToS to inactive.
     /*@Test
     public void noActiveTermsOfServiceTest(){
-        given().when().get("talent/ws/content/privacyPolicy").then().statusCode(204);
+        given().queryParam("platform","web").when().get("talent/ws/content/privacyPolicy").then().statusCode(204);
     }*/
 
 }
--- talent-ws/src/test/java/cr/talent/ws/rest/TermsOfServiceResourceTest.java
+++ talent-ws/src/test/java/cr/talent/ws/rest/TermsOfServiceResourceTest.java
@@ -11,14 +11,29 @@ public class TermsOfServiceResourceTest extends FunctionalTest {
     }
 
     @Test
-    public void pingTermsOfService() {
-        given().when().get("/ws/content/termsOfService").then().statusCode(200);
+    public void nullPlatformRequestTest(){
+        given().when().get("/ws/content/termsOfService").then().statusCode(400);
+    }
+
+    @Test
+    public void emptyPlatformRequestTest(){
+        given().queryParam("platform","").when().get("/ws/content/termsOfService").then().statusCode(400);
+    }
+
+    @Test
+    public void testWebTermsOfService() {
+        given().queryParam("platform","web").when().get("/ws/content/termsOfService").then().statusCode(200);
+    }
+
+    @Test
+    public void testAndroidTermsOfService() {
+        given().queryParam("platform","android").when().get("/ws/content/termsOfService").then().statusCode(200);
     }
 
     //The following test was run on localhost, by setting the active PP to inactive.
     /*@Test
     public void noActiveTermsOfServiceTest(){
-        given().when().get("talent/ws/content/termsOfService").then().statusCode(204);
+        given().queryParam("platform","web").when()when().get("talent/ws/content/termsOfService").then().statusCode(204);
     }*/
 
 }
