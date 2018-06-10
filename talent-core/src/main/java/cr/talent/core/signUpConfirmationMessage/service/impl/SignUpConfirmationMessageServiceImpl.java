bddce9766c40deeb4f8da5848e418476a685ffa5
daniel.montesdeoca <danmdob@gmail.com>
2018-06-09 23:40:00 -0600

    TAL-335 BE | Sign Up Step 2
    Made fixes and improved documentation

--- talent-core/src/main/java/cr/talent/core/signUpConfirmationMessage/service/impl/SignUpConfirmationMessageServiceImpl.java
+++ talent-core/src/main/java/cr/talent/core/signUpConfirmationMessage/service/impl/SignUpConfirmationMessageServiceImpl.java
@@ -7,9 +7,11 @@ import cr.talent.core.signUpConfirmationMessage.service.SignUpConfirmationMessag
 import cr.talent.model.SignUpConfirmationMessage;
 import cr.talent.model.TechnicalResource;
 import cr.talent.model.User;
+import cr.talent.support.SecurityUtils;
 import cr.talent.support.exceptions.NonExistentConfirmationMessageException;
 import cr.talent.support.service.impl.CrudServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -23,14 +25,30 @@ import javax.transaction.Transactional;
 @Transactional
 public class SignUpConfirmationMessageServiceImpl extends CrudServiceImpl<SignUpConfirmationMessage, String> implements SignUpConfirmationMessageService {
 
+    /**
+     * Data access object reference to perform SignUpConfirmationMessage operations.
+     */
     @Autowired
     private SignUpConfirmationMessageDao signUpConfirmationMessageDao;
 
+    /**
+     * Used to perform operations with technical resources
+     */
     @Autowired
     private TechnicalResourceService technicalResourceService;
 
+    /**
+     * Used to send confirmation emails for the first step of the sign up
+     */
     @Autowired
     SignUpConfirmationEmailService signUpConfirmationEmailService;
+
+    /**
+     * Password encoder provided by spring to cipher a user's password and store it in the database so that it can
+     * be compared later when the user enters his password.
+     */
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     //The highest number that can be used for a confirmation code
     private static final int MAX_CONFIRMATION_CODE = 999999;
@@ -41,6 +59,9 @@ public class SignUpConfirmationMessageServiceImpl extends CrudServiceImpl<SignUp
     public void init() { setCrudDao(this.signUpConfirmationMessageDao); }
 
     @Override
+    /**
+     * @see cr.talent.core.signUpConfirmationMessage.service.SignUpConfirmationMessageService#getActiveConfirmationMessage(String)
+     */
     public SignUpConfirmationMessage getActiveConfirmationMessage(String username) {
         SignUpConfirmationMessage signUpConfirmation = signUpConfirmationMessageDao.getActiveConfirmationMessage(username);
 
@@ -48,6 +69,9 @@ public class SignUpConfirmationMessageServiceImpl extends CrudServiceImpl<SignUp
     }
 
     @Override
+    /**
+     * @see cr.talent.core.signUpConfirmationMessage.service.SignUpConfirmationMessageService#sendMessage(String, String, String, String)
+     */
     public void sendMessage(String firstName, String lastName, String username, String password) {
         TechnicalResource technicalResource;
         SignUpConfirmationMessage signUpConfirmationMessage;
@@ -63,10 +87,11 @@ public class SignUpConfirmationMessageServiceImpl extends CrudServiceImpl<SignUp
             hadAnotherConfirmationMessage = true;
         }
 
+        SecurityUtils.validatePassword(password); //throws IllegalArgumentException if it is not valid
         technicalResource.setFirstName(firstName);
         technicalResource.setLastName(lastName);
         technicalResource.setUsername(username);
-        technicalResource.setPassword(password);
+        technicalResource.setPassword(this.passwordEncoder.encode(password)); // the password is encoded
         technicalResource.setStatus(User.Status.INACTIVE);
         technicalResource.setAdministrator(true); //because they are signing up while creating an organization
 
@@ -84,17 +109,20 @@ public class SignUpConfirmationMessageServiceImpl extends CrudServiceImpl<SignUp
     }
 
     @Override
+    /**
+     * @see cr.talent.core.signUpConfirmationMessage.service.SignUpConfirmationMessageService#confirmEmail(String, String)
+     */
     public boolean confirmEmail(String code, String username) {
         SignUpConfirmationMessage signUpConfirmationMessage = this.getActiveConfirmationMessage(username);
         if (signUpConfirmationMessage == null)
             throw new NonExistentConfirmationMessageException();
 
         boolean codeMatchesConfirmationMessage = code.equals(signUpConfirmationMessage.getConfirmationCode());
-        TechnicalResource technicalResource = signUpConfirmationMessage.getTechnicalResource();
-
         if (codeMatchesConfirmationMessage) {
+            TechnicalResource technicalResource = signUpConfirmationMessage.getTechnicalResource();
             super.remove(signUpConfirmationMessage);
             technicalResource.setStatus(User.Status.ACTIVE);
+            technicalResourceService.update(technicalResource);
         }
 
         return codeMatchesConfirmationMessage;
