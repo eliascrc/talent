051a673b3895e34e915cbea7bccf363bc4c4e77d
Fabian Roberto Leandro <roleandroflores12@gmail.com>
2018-06-28 14:30:05 -0600

    TAL-427 BE | Assign a technical position to a technical resource
    Corrected an error in the CareerPathSevice implementation.

--- talent-core/src/main/java/cr/talent/core/careerPath/service/impl/CareerPathServiceImpl.java
+++ talent-core/src/main/java/cr/talent/core/careerPath/service/impl/CareerPathServiceImpl.java
@@ -1,8 +1,10 @@
 package cr.talent.core.careerPath.service.impl;
 
+import cr.talent.core.careerPath.dao.CareerPathDao;
 import cr.talent.core.careerPath.service.CareerPathService;
 import cr.talent.model.CareerPath;
 import cr.talent.support.service.impl.CrudServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -15,4 +17,11 @@ import javax.transaction.Transactional;
 @Service("careerPathService")
 @Transactional
 public class CareerPathServiceImpl extends CrudServiceImpl<CareerPath, String> implements CareerPathService {
+
+    @Autowired
+    CareerPathDao careerPathDao;
+
+    public void init() {
+        setCrudDao(this.careerPathDao);
+    }
 }
