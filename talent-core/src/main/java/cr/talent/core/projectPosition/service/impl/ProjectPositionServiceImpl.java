5ea928c6b65877724ed889294b3e73dc562c233f
JosueCubero <jo96cube@gmail.com>
2018-06-30 15:24:10 -0600

    TAL-441 BE | Get technical resource’s project list.

--- talent-core/src/main/java/cr/talent/core/projectPosition/service/impl/ProjectPositionServiceImpl.java
+++ talent-core/src/main/java/cr/talent/core/projectPosition/service/impl/ProjectPositionServiceImpl.java
@@ -2,16 +2,22 @@ package cr.talent.core.projectPosition.service.impl;
 
 import cr.talent.core.projectPosition.dao.ProjectPositionDao;
 import cr.talent.core.projectPosition.service.ProjectPositionService;
+import cr.talent.model.Project;
 import cr.talent.model.ProjectPosition;
+import cr.talent.model.ProjectPositionHolder;
+import cr.talent.model.TechnicalResource;
+import cr.talent.support.exceptions.NoActiveTechnicalResourceProjectException;
+import cr.talent.support.flexjson.JSONSerializerBuilder;
 import cr.talent.support.service.impl.CrudServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import java.util.*;
 
 /**
  * Default implementation of the {@link cr.talent.core.projectPosition.service.ProjectPositionService}.
  *
- * @author Elías Calderón
+ * @author Elías Calderón, Josue Cubero
  */
 @Service("projectPositionService")
 @Transactional
@@ -24,4 +30,38 @@ public class ProjectPositionServiceImpl extends CrudServiceImpl<ProjectPosition,
         setCrudDao(this.projectPositionDao);
     }
 
+    /**
+     * @see cr.talent.core.projectPosition.service.ProjectPositionService#getTechnicalResourceActiveProjects(TechnicalResource)
+     */
+    @Override
+    public String getTechnicalResourceActiveProjects(TechnicalResource technicalResource) {
+        final String noActiveTechnicalResourceProjectsMsg = technicalResource.getFirstName() + " " + technicalResource.getLastName()
+                + " does not have any active project";
+
+        if(technicalResource.getProjectPositions().isEmpty()) //ask first for effiency reasons
+            throw new NoActiveTechnicalResourceProjectException(noActiveTechnicalResourceProjectsMsg);
+
+        Set<ProjectPositionHolder> projectPositionHolders = technicalResource.getProjectPositions();
+        List<ProjectPositionHolder> activeProjectPositionHolders = new ArrayList<>();
+
+        Iterator<ProjectPositionHolder> projectPositionHolderIterator = projectPositionHolders.iterator();
+        ProjectPositionHolder projectPositionHolder;
+        while(projectPositionHolderIterator.hasNext()){ //iterate projectPositionHolders for active ones
+            projectPositionHolder = projectPositionHolderIterator.next();
+            if(projectPositionHolder.isActive()){
+                activeProjectPositionHolders.add(projectPositionHolder);
+            }
+        }
+
+        if(activeProjectPositionHolders.isEmpty()) //ask again after the iteration
+            throw new NoActiveTechnicalResourceProjectException(noActiveTechnicalResourceProjectsMsg);
+
+        List<Project> projects = new ArrayList<>();
+        for (ProjectPositionHolder activeProjectPositionHolder : activeProjectPositionHolders) {
+            projects.add(activeProjectPositionHolder.getProjectPosition().getProject());
+        }
+
+        return JSONSerializerBuilder.getTechnicalResourceActiveProjectsSerializer().serialize(projects);
+
+    }
 }
---
+++ talent-ws/src/main/java/cr/talent/ws/rest/TechnicalResourceProjectResource.java
@@ -0,0 +1,60 @@
+package cr.talent.ws.rest;
+
+import cr.talent.core.projectPosition.service.ProjectPositionService;
+import cr.talent.core.security.technicalResource.service.TechnicalResourceService;
+import cr.talent.model.TechnicalResource;
+import cr.talent.support.SecurityUtils;
+import cr.talent.support.exceptions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ * Resource with one POST endpoint that handles the query for a technical resource active project list.
+ *
+ * @author Josue Cubero
+ */
+@Component
+@Scope("request")
+@Path("technicalResource/project")
+public class TechnicalResourceProjectResource {
+
+    @Autowired
+    ProjectPositionService projectPositionService;
+
+    @Autowired
+    TechnicalResourceService technicalResourceService;
+
+    /**
+     * Receives a request to query the active projects of a technical resource.
+     *
+     * @return 204 if the technical resource does not have any active project.
+     *         401 if no user is logged in
+     *         200 if the active projects are correctly queried and retrieved.
+     */
+    @GET
+    @Path("/get")
+    public Response getActiveProjects() {
+
+        TechnicalResource lazySessionTechnicalResource = SecurityUtils.getLoggedInTechnicalResource();
+
+        TechnicalResource technicalResource =
+                this.technicalResourceService.getTechnicalResourceByUsernameAndOrganizationIdentifier(
+                        lazySessionTechnicalResource.getUsername(),
+                        lazySessionTechnicalResource.getOrganization().getUniqueIdentifier());
+
+        if(technicalResource == null)
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+
+        try {
+            return Response.ok().entity(this.projectPositionService.getTechnicalResourceActiveProjects(technicalResource)).build();
+        } catch (NoActiveTechnicalResourceProjectException e) {
+            return Response.status(Response.Status.NO_CONTENT).
+                    entity(e.getMessage()).build();
+        }
+    }
+}
