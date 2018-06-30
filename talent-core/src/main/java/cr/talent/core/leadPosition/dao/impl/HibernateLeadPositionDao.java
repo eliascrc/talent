80c773a40c55ec02dd664fc4c8b1cbc7d0400d69
JosueCubero <jo96cube@gmail.com>
2018-06-30 00:03:39 -0600

    TAL-470 | BE | Create a Project

---
+++ talent-core/src/main/java/cr/talent/core/leadPosition/dao/impl/HibernateLeadPositionDao.java
@@ -0,0 +1,24 @@
+package cr.talent.core.leadPosition.dao.impl;
+
+import cr.talent.core.leadPosition.dao.LeadPositionDao;
+import cr.talent.model.LeadPosition;
+import cr.talent.support.dao.impl.HibernateCrudDao;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Hibernate implementation of the {@link LeadPositionDao}.
+ *
+ * @author Josue Cubero
+ */
+@Repository("leadPositionDao")
+public class HibernateLeadPositionDao extends HibernateCrudDao<LeadPosition, String> implements LeadPositionDao {
+
+    @Autowired
+    public HibernateLeadPositionDao(@Qualifier("sessionFactory")SessionFactory sessionFactory) {
+        setSessionFactory(sessionFactory);
+    }
+
+}
