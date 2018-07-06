8e7c31e8980eb3b4ae198c9efa45e5ead700ebb3
daniel.montesdeoca <danmdob@gmail.com>
2018-07-06 15:41:08 -0600

    TAL-425 BE | Edit technical resource’s basic information Implemented the ws for editing a resource's basic information, one endpoint is for administrators and the other for normal users to edit their nicknames

--- talent-ws/src/main/java/cr/talent/ws/rest/TechnicalResourceResource.java
+++ talent-ws/src/main/java/cr/talent/ws/rest/TechnicalResourceResource.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -85,6 +82,77 @@ public class TechnicalResourceResource {
             return Response.status(Response.Status.NOT_FOUND).build();
 
         String serializedResource = JSONSerializerBuilder.getTechnicalResourceSerializer().serialize(technicalResource);
+        return Response.ok().entity(serializedResource).build();
+    }
+
+    /**
+     * Post endpoint for editing the basic information of a technical resource
+     * @param technicalResourceId the identifier of the technical resource
+     * @param firstName the new first name for the technical resource
+     * @param lastName the new last name for the technical resource
+     * @param nickname the new nickname for the technical resource
+     *
+     * @return  400 if a parameter was not sent or sent empty, or if the user performing the request does not belong to
+     *              an organization
+     *          403 if the resource is not the administrator of the organization
+     *          404 if no resource was found with the provided identifier or if the resource that was found belongs to
+     *              another organization
+     *          200 and a JSON with the user information if the request was successful
+     */
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/basicInformation/edit")
+    public Response editBasicInformation(@FormParam("technicalResourceId") String technicalResourceId,
+                                         @FormParam("firstName") String firstName,
+                                         @FormParam("lastName") String lastName,
+                                         @FormParam("nickname") String nickname) {
+        if (StringUtils.isEmpty(technicalResourceId) || StringUtils.isEmpty(firstName) || StringUtils.isEmpty(nickname))
+            return Response.status(Response.Status.BAD_REQUEST).entity("A parameter was left empty or not sent")
+                    .build();
+
+        TechnicalResource loggedInTechnicalResource = SecurityUtils.getLoggedInTechnicalResource();
+
+        if (loggedInTechnicalResource.getOrganization() == null)
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("You must belong to an organization to edit resources").build();
+
+        if (!loggedInTechnicalResource.isAdministrator())
+            return Response.status(Response.Status.FORBIDDEN).build();
+
+        TechnicalResource technicalResource = technicalResourceService.findById(technicalResourceId);
+
+        if (technicalResource == null || !loggedInTechnicalResource.getOrganization().equals(technicalResource.getOrganization()))
+            return Response.status(Response.Status.NOT_FOUND).build();
+
+        this.technicalResourceService.editBasicInformation(technicalResource, firstName, lastName, nickname);
+
+        String serializedResource = JSONSerializerBuilder.getTechnicalResourceSerializer().serialize(technicalResource);
+        return Response.ok().entity(serializedResource).build();
+    }
+
+    /**
+     * Post endpoint for editing the basic information of the logged in technical resource
+     * @param nickname the new nickname for the technical resource
+     *
+     * @return  400 if a parameter was not sent or sent empty
+     *          200 and a JSON with the user information if the request was successful
+     */
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/basicInformation/edit/nickname")
+    public Response editBasicInformation(@FormParam("nickname") String nickname) {
+        if (StringUtils.isEmpty(nickname))
+            return Response.status(Response.Status.BAD_REQUEST).entity("A parameter was left empty or not sent")
+                    .build();
+
+        TechnicalResource loggedInTechnicalResource = SecurityUtils.getLoggedInTechnicalResource();
+
+        this.technicalResourceService.editBasicInformation(loggedInTechnicalResource,
+                loggedInTechnicalResource.getFirstName(), loggedInTechnicalResource.getLastName(), nickname);
+
+        String serializedResource = JSONSerializerBuilder.getTechnicalResourceSerializer()
+                .serialize(loggedInTechnicalResource);
+
         return Response.ok().entity(serializedResource).build();
     }
 
